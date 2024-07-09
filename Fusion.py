2647cdfe46ebd78f3d507a3e05b03c2212ea24e2
fbbgood <127197507+fbbgood@users.noreply.github.com>
2024-07-09 11:22:49 +0800

    Update Fusion.py

--- Fusion.py
+++ Fusion.py
@@ -4,26 +4,6 @@ from torch.nn import MultiheadAttention
 from typing import Optional, Tuple
 
 
-
-    def forward(self, v, q):
-        v = self.v_net(v)
-        q = self.q_net(q)
-
-        # MultiheadAttention requires input shape (sequence_length, h_dim)
-        v = v.transpose(0, 1)
-        q = q.transpose(0, 1)
-
-        attn_output, attn_weights = self.multihead_attention(q, v, v)
-
-        # Transpose back to original shape (h_dim, sequence_length, )
-        attn_output = attn_output.transpose(0, 1)
-        attn_weights = attn_weights.transpose(0, 1)
-        # Apply Pooling
-        if self.pooling is not None:
-            attn_output = attn_output.permute(0, 2, 1)
-            attn_output = self.pooling(attn_output)
-            attn_output = attn_output.permute(0, 2, 1)
-
         output = self.output_net(attn_output)
 
         return output, attn_weights
